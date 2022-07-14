036eb6d87801e786bac5258a7655b4530527398a
unknown <thithibuibui1@gmail.com>
2022-07-14 20:26:58 +0700

    test

--- Program.cs
+++
@@ -1,4 +0,0 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Feature 2");
-Console.WriteLine("Feature 3");
--- add.cpp
+++ add.cpp
@@ -1,1 +1,11 @@
-#include 
+#include <iostream>
+using namespace std ;
+int main() { 
+    cout<<"Hello Moi Nguoi ";
+    int sum =1 ;
+    for(int i= 0 ;i<10;i++ ){
+        sum += i;
+    }
+    cout<<sum;  
+
+}
