b11bff5e758902f4b4f5be03e459e8092f2f34e0
unknown <thithibuibui1@gmail.com>
2022-07-14 20:38:02 +0700

    Them chuc nang tinh tong 2 so

--- add.cpp
+++ add.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 using namespace std ;
-int main() { 
-    cout<<"Hello Moi Nguoi ";
-    int sum =1 ;
-    for(int i= 0 ;i<10;i++ ){
-        sum += i;
-    }
-    cout<<sum;  
+int TongHaiSo(int a , int b ){
+    return a+b;
+}
+int main() {  
 
 }
