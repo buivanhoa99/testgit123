867d172ace036a34dd60c775d11601e6aa3ac9a4
Bui Van Hoa <hoa.bui@saigontechnology.com>
2022-07-14 20:25:58 +0700

    Them ham MAX

--- Program.cs
+++ Program.cs
@@ -2,3 +2,9 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Feature 2");
 Console.WriteLine("Feature 3");
+public class Math{
+    public int Max(int a, int b){
+        if (a > b) return a;
+        return b;
+    }
+}
